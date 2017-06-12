08e57ed37f5d65a8f823c9554132eda1c04287c5
chengzhx76 <chengzhx76@qq.com>
2017-06-12 16:22:07 +0800

    增加区块链的Servlet

---
+++ src/main/java/com/cheng/servletmvc/Chain.java
@@ -0,0 +1,10 @@
+package com.cheng.servletmvc;
+
+/**
+ * Desc: 区块链相关
+ * Author: hp
+ * Date: 2017/6/12
+ */
+public class Chain extends BaseServlet {
+
+}
--- src/test/java/com/cheng/other/test/RedFile.java
+++ src/test/java/com/cheng/other/test/RedFile.java
@@ -5,9 +5,7 @@ import com.google.common.io.Files;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Desc:
@@ -24,12 +22,14 @@ public class RedFile {
         File srcFile = new File("E:\\Idea\\ServletMVC\\src\\test\\resources\\sql.txt");
         List<String> contents = Files.readLines(srcFile, Charsets.UTF_8);
 
-        Set<String> sqls = new HashSet<>();
-        for (String content : contents) {
-            sqls.add(content.trim());
-        }
-        for (int i=1; i<=sqls.size(); i++) {
-            System.out.println("('"+i+"','"+contents.get(i-1)+"','1','9999','9999'),");
+//        Set<String> sqls = new HashSet<>();
+//        for (String content : contents) {
+//            sqls.add(content.trim());
+//        }
+        int count = 913;
+        for (int i=1; i<=contents.size(); i++) {
+            System.out.println("('"+count+"','"+contents.get(i-1)+"','1','9999','9999','ORACLE'),");
+            count++;
         }
 
         //for (String string : contents) {
